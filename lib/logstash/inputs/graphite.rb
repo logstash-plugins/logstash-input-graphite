f11f9039c5b64d70057891bf4d3ce8d5daec4e1a
Suyog Rao <suyog.rao@gmail.com>
2014-11-06 14:03:27 -0800

    Added inline code asciidoc formatting

--- lib/logstash/inputs/graphite.rb
+++ lib/logstash/inputs/graphite.rb
@@ -4,16 +4,16 @@ require "logstash/namespace"
 require "logstash/timestamp"
 
 # Receive graphite metrics. This plugin understands the text-based graphite
-# carbon protocol. Both 'N' and specific-timestamp forms are supported, example:
-#
+# carbon protocol. Both `N` and `specific-timestamp` forms are supported, example:
+# [source,ruby]
 #     mysql.slow_query.count 204 N
 #     haproxy.live_backends 7 1364608909
 #
-# 'N' means 'now' for a timestamp. This plugin also supports having the time
+# `N` means `now` for a timestamp. This plugin also supports having the time
 # specified in the metric payload:
 #
 # For every metric received from a client, a single event will be emitted with
-# the metric name as the field (like 'mysql.slow_query.count') and the metric
+# the metric name as the field (like `mysql.slow_query.count`) and the metric
 # value as the field's value.
 class LogStash::Inputs::Graphite < LogStash::Inputs::Tcp
   config_name "graphite"
